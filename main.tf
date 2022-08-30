1cdb5b6be4bb42993579c27af69355e24f81165d
Ravie kumarr <raviga006@gmail.com>
2022-08-30 11:03:28 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,75 @@
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#*                 Root Module                         *#
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+# Terraform Block
+
+terraform {
+    required_providers  {
+         azurerm =   {
+            source  =   "hashicorp/azurerm"
+        }
+    }
+    backend "azurerm" {
+        resource_group_name = "polo"
+        storage_account_name = "terrasave"
+        container_name = "tfsave"
+        key = "terraform.tfstate"
+    
+      
+    }
+}
+
+# Provider Block
+
+provider "azurerm" {
+    version         =   "~> 2.0"
+    client_id       =   var.client_id
+    client_secret   =   var.client_secret
+    subscription_id =   var.subscription_id
+    tenant_id       =   var.tenant_id
+    
+    features {}
+}
+
+
+
+
+provider "azuread" {
+    version         =   ">= 0.11"
+    client_id       =   var.client_id
+    client_secret   =   var.client_secret
+    tenant_id       =   var.tenant_id
+    alias           =   "ad"
+}
+
+
+// module "windows-server" {
+//     source  =   "./azure/WindowsServer2019"
+// }
+
+// module "winvm" {
+//     source =    "./azure/Windows10-ExistingInfra"
+// }
+
+
+
+// module "aks" {
+//     source    =     "./azure/aks"
+//     env       =     "dev"
+// }
+
+
+
+// module "customrole" {
+//     source  =   "./azure/custom-roles"
+// }
+
+// module "provisioners" {
+//     source  =   "./azure/provisioners-example"
+// }
+
+// module "linuxvm" {
+//     source  =   "./azure/linuxVM"
+// }
+
