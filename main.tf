0a71c1e66b0b05d9c18fd9db330626d73eb6c0c8
Ravie kumarr Sapram <70814918+raviga006@users.noreply.github.com>
2022-09-04 18:11:49 +0530

    Stroage keys
    storage keus

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
     backend "azurerm" {
         resource_group_name = "polo"
         storage_account_name = "terrasave"
-        container_name = "tfsave"
+        container_name = "tfback"
         key = "terraform.tfstate"
     
       
