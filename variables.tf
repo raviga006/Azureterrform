97be45990291a5d25381534f9182aa7878784f1f
Ravie kumarr Sapram <70814918+raviga006@users.noreply.github.com>
2022-09-04 18:00:58 +0530

    updated
    check

--- variables.tf
+++ variables.tf
@@ -5,21 +5,21 @@
 # Service Principal Variables
 
 variable "client_id" {
-    description =   "Client ID (APP ID) of the application"
+    description =   "4286d482-63eb-4183-bf00-3c9e3ea89688"
     type        =   string
 }
 
 variable "client_secret" {
-    description =   "Client Secret (Password) of the application"
+    description =   "fFU8Q~JaU2vvYDThHUPLPibmDOV3NAsDLE3kqaph"
     type        =   string
 }
 
 variable "subscription_id" {
-    description =   "Subscription ID"
+    description =   "0a396cb4-0e8c-43fe-86fc-346ce9f7dbb6"
     type        =   string
 }
 
 variable "tenant_id" {
-    description =   "Tenant ID"
+    description =   "b9fec68c-c92d-461e-9a97-3d03a0f18b82"
     type        =   string
 }
